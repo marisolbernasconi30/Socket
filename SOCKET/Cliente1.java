1cad364fdcffe558cc6b20b1ab3cee39f5630f32
Marisol Bernasconi <usa2018sb1030@gmail.com>
2025-07-15 17:38:06 -0300

    basico interfaz

--- SOCKET/Cliente1.java
+++ SOCKET/Cliente1.java
@@ -1,6 +1,58 @@
 package SOCKET;
 
-public class Cliente1{
+import javax.swing.*;
 
 
+public class Cliente1 {
+
+	public static void main(String[] args) {
+		
+		
+		MarcoCliente mimarco=new MarcoCliente();
+		
+		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
+
+
+class MarcoCliente extends JFrame{
+	
+	public MarcoCliente(){
+		
+		setBounds(600,300,280,350);
+				
+		LaminaMarcoCliente milamina=new LaminaMarcoCliente();
+		
+		add(milamina);
+		
+		setVisible(true);
+		}	
+	
+}
+
+class LaminaMarcoCliente extends JPanel{
+	
+	public LaminaMarcoCliente(){
+	
+		JLabel texto=new JLabel("CLIENTE");
+		
+		add(texto);
+	
+		campo1=new JTextField(20);
+	
+		add(campo1);		
+	
+		miboton=new JButton("Enviar");
+		
+		add(miboton);	
+		
+	}
+		
+		
+	private JTextField campo1;
+	
+	private JButton miboton;
+	
 }
--- SOCKET/Servidor2.java
+++ SOCKET/Servidor2.java
@@ -1,5 +1,39 @@
 package SOCKET;
 
-public class Servidor2 {
-    
+import javax.swing.*;
+
+import java.awt.*;
+
+public class Servidor2  {
+
+	public static void main(String[] args) {
+		
+		MarcoServidor mimarco=new MarcoServidor();
+		
+		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+	}	
+}
+
+class MarcoServidor extends JFrame {
+	
+	public MarcoServidor(){
+		
+		setBounds(1200,300,280,350);				
+			
+		JPanel milamina= new JPanel();
+		
+		milamina.setLayout(new BorderLayout());
+		
+		areatexto=new JTextArea();
+		
+		milamina.add(areatexto,BorderLayout.CENTER);
+		
+		add(milamina);
+		
+		setVisible(true);
+		
+		}
+	
+	private	JTextArea areatexto;
 }
